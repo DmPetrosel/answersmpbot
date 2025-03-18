4f352091f273e7ef6baa42900d2e570c4a2d2711
Dmitriy Petrosel <Dm.Petroselinum@gmail.com>
2025-03-18 14:10:14 +0300

    fix choose bot

--- app/keyboard/marketer_kb.py
+++ app/keyboard/marketer_kb.py
@@ -19,8 +19,8 @@ async def add_manager_list_kb(bot_username :str):
     kb.inline_keyboard.append([InlineKeyboardButton(text='Отмена', callback_data='mccancel_call')])
     return kb        
 
-def choose_bot_for_add_manager_kb(chat_id:str):
-    user_bot_list = get_all_bots(chat_id=chat_id)
+async def choose_bot_for_add_manager_kb(chat_id:str):
+    user_bot_list = await get_all_bots(chat_id=chat_id)
     kb = InlineKeyboardMarkup(inline_keyboard=[])
     if user_bot_list:
         for ibot in user_bot_list:
