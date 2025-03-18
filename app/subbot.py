65fcd4cda4a71ff250af900ea65a7751e3e4491d
Dmitriy Petrosel <Dm.Petroselinum@gmail.com>
2025-03-18 14:17:49 +0300

    detalize sended

--- app/subbot.py
+++ app/subbot.py
@@ -100,7 +100,7 @@ async def sbb_callbacks(callback: types.CallbackQuery, state: FSMContext, bot: M
             bot_info = await get_one_bot(bot_username=(await bot.get_me()).username)
             success = await answer_for_feedback(wb_token=bot_info.wb_token, feedback_id=question.feed_id, text=ans.text)
             if success:
-                await callback.message.edit_text('Ответ отправлен')
+                await callback.message.edit_text(f'✅ Ответ отправлен\n\n{question.feed_mess}\n\n✉️ {question.feed_ans}')
                 await update_wbfeed(id=question.id, is_answering=False, feed_ans=ans.text)
                 mess_ids = []
                 mess_ids = [[m.chat_id, m.mess_id] for m in await get_all_wbfeedanswer(question_id=question_id)]
@@ -201,7 +201,7 @@ is_notified_balance_list = {}
 
 async def nmain_loop(bot: MyBot, main_bot: MyBot):
     bot_username = (await bot.get_me()).username
-    n = await get_bot_row(bot_username=bot_username)
+    n = await get_bot_row(bot_username=bot_username)await
     bot_info = await get_one_bot(bot_username=bot_username)
     is_notified_auth_list[bot_info.chat_id] = False
     is_notified_balance_list[bot_info.chat_id] = False
