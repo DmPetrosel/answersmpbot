6bd794b9d573a1910084852fdfd908a9cfe169a0
Dmitriy Petrosel <Dm.Petroselinum@gmail.com>
2025-03-18 13:45:56 +0300

    3->5

--- app/func/wb_feedback.py
+++ app/func/wb_feedback.py
@@ -29,7 +29,7 @@ import aiohttp
 wb_feedbacks_link = 'https://feedbacks-api-sandbox.wildberries.ru'
 # wb_feedbacks_link ='https://feedbacks-api.wildberries.ru' # Original
 class WBFeedback:
-    def __init__(self, bot_username, bot, interval=1, quantity_of_cards=3):
+    def __init__(self, bot_username, bot, interval=1, quantity_of_cards=5):
         # super().__init__()
         # self.daemon = True
         self.interval = interval
