0fd7e56922ed46831ec278cac75d2d76a3d07711
Dmitriy Petrosel <Dm.Petroselinum@gmail.com>
2025-04-06 19:56:46 +0300

    fix to new api

--- app/func/wbstat.py
+++ app/func/wbstat.py
@@ -20,47 +20,73 @@ class WBStat:
         self.bot_username = bot_username
         self.daemon = daemon
         self.interval = 30
-    async def request_for_stocks(self):
-        url = f"{base_url}/api/v1/warehouse_remains?groupByNm=true&groupBySubject=true"
-        headers = {'Authorization': self.token, 'Content-Type': 'application/json'}
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, headers=headers) as response:
-                if response.status == 200:
-                    data = await response.json()
-                    taskId = data.get("data").get('taskId')
-                    print(f"taskId = {taskId}")
-                    return taskId
-                else:
-                    print(f"request for stock {response.status}")
-            return None
+    # async def request_for_stocks(self):
+    #     url = f"{base_url}/api/v1/warehouse_remains?groupByNm=true&groupBySubject=true"
+    #     headers = {'Authorization': self.token, 'Content-Type': 'application/json'}
+    #     async with aiohttp.ClientSession() as session:
+    #         async with session.get(url, headers=headers) as response:
+    #             if response.status == 200:
+    #                 data = await response.json()
+    #                 taskId = data.get("data").get('taskId')
+    #                 print(f"taskId = {taskId}")
+    #                 return taskId
+    #             else:
+    #                 print(f"request for stock {response.status}")
+    #         return None
 
-    async def check_status(self, taskId):
-        url = f"{base_url}/api/v1/warehouse_remains/tasks/{taskId}/status"
-        headers = {'Authorization': self.token, 'Content-Type': 'application/json'}
-        status = None
-        i = 0
-        async with aiohttp.ClientSession() as session:
-            while (status != 'done') and i < 40:
-                async with session.get(url, headers=headers) as response:
+    # async def check_status(self, taskId):
+    #     url = f"{base_url}/api/v1/warehouse_remains/tasks/{taskId}/status"
+    #     headers = {'Authorization': self.token, 'Content-Type': 'application/json'}
+    #     status = None
+    #     i = 0
+    #     async with aiohttp.ClientSession() as session:
+    #         while (status != 'done') and i < 40:
+    #             async with session.get(url, headers=headers) as response:
+    #                 if response.status == 200:
+    #                     data = await response.json()
+    #                     status = data.get("data").get('status')
+    #                     print(f'status = {status}')
+    #                 else: print(f"check_status:{response.status} {await response.json()}")
+    #                 await asyncio.sleep(3)
+    #                 i +=1
+    #         if status == 'done': return True
+    #         else: return False
+
+    # async def get_stocks(self, taskId):
+    #     url = f"{base_url}/api/v1/warehouse_remains/tasks/{taskId}/download"
+    #     headers = {'Authorization': self.token, 'Content-Type': 'application/json'}
+    #     async with aiohttp.ClientSession() as session:
+    #         async with session.get(url, headers=headers) as response:
+    #             if response.status == 200:
+    #                 data = await response.json()
+    #                 return data
+    #         return None
+    async def get_stock(self, wb_token: str):
+        date = (datetime.now()).strftime('%Y-%m-%d')
+        headers = {'Authorization': wb_token}
+
+        json_data = []
+        lastChangeDate = date
+        data = ["data"]
+        while data != []:
+            url = f'https://statistics-api.wildberries.ru/api/v1/supplier/stocks?dateFrom={lastChangeDate}'
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url=url, headers=headers) as response:
+                    data = await response.json()
                     if response.status == 200:
-                        data = await response.json()
-                        status = data.get("data").get('status')
-                        print(f'status = {status}')
-                    else: print(f"check_status:{response.status} {await response.json()}")
-                    await asyncio.sleep(3)
-                    i +=1
-            if status == 'done': return True
-            else: return False
-
-    async def get_stocks(self, taskId):
-        url = f"{base_url}/api/v1/warehouse_remains/tasks/{taskId}/download"
-        headers = {'Authorization': self.token, 'Content-Type': 'application/json'}
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, headers=headers) as response:
-                if response.status == 200:
-                    data = await response.json()
-                    return data
-            return None
+                        for row in data:
+                            lastChangeDate = row["lastChangeDate"]
+                            nmId = row["nmId"]
+                            quantityFull = row["quantityFull"]
+                            json_data.append({"nmId": nmId, "quantityFull": quantityFull, 'subject':row['subject']})
+                        if len(data)>=60000:
+                            await asyncio.sleep(60)
+                        else:
+                            break
+                    else:
+                        logging.error("Stocks data was not obtain fully. Status {status}".format(status=response.status))
+                        break
+        return json_data
 
     async def write_to_file(self, data):
         with open(f"data/{self.bot_username}_stocks.json", 'w', encoding='utf-8') as f:
@@ -93,11 +119,16 @@ class WBStat:
         while True:
             logging.info('STOCKS UPDATING...')
             await self.get_wb_token_loop()
-            taskId = await self.request_for_stocks()
-            print(taskId)
-            if await self.check_status(taskId):
-                data = await self.get_stocks(taskId)
-                print(data)
+            # self.token = 'eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjUwMjE3djEiLCJ0eXAiOiJKV1QifQ.eyJlbnQiOjEsImV4cCI6MTc1NjI1MDkyMiwiaWQiOiIwMTk1M2NkZC1jODllLTczNTQtYmUyNS04NzE0NzJlZmRhNDEiLCJpaWQiOjQ5NjE0OTU0LCJvaWQiOjIwNjMxMSwicyI6MTA3Mzc0OTc1OCwic2lkIjoiZGE4ZjBlNGYtMjQ3MC00MzFhLWI2MTEtMTA2YmJhYjMyYzM3IiwidCI6ZmFsc2UsInVpZCI6NDk2MTQ5NTR9.gESAyZPaSJZChRadpb7iMnWi9gr1UG73-vo_al55xJkEetqdARMBBHqUz9mvEkuDdwY7AfHoVjH80TKta5kl4Q'
+            # taskId = await self.request_for_stocks()
+            # print(taskId)
+            # if await self.check_status(taskId):
+            #     data = await self.get_stocks(taskId)
+            #     print(data)
+            #     data = self.extendtofull(data)
+            #     await self.write_to_file(data)
+            data = await self.get_stock(self.token) 
+            if data:
                 await self.write_to_file(data)
                 print('\ndata obtained\n')
             else:
@@ -125,13 +156,13 @@ def get_random_three_str(
         while j > 0 and len(data) > 0:
             j -= 1
             r = rand.randint(0, len(data)-1)
-            if data[r]["quantityWarehousesFull"] > 0 and data[r]["nmID"] != current_nmId:
+            if data[r]["quantityFull"] > 0 and data[r]["nmId"] != current_nmId:
                 break
             else:
                 continue
-        if data[r]["quantityWarehousesFull"] == 0 or data[r]["nmID"] == current_nmId:
+        if data[r]["quantityFull"] == 0 or data[r]["nmId"] == current_nmId:
             break
-        rand_data.append(f'{data[r]["subjectName"]} Арт. {data[r]["nmId"]}')
+        rand_data.append(f'{data[r]["subject"]} Арт. {data[r]["nmId"]}')
         del data[r]
     random_three_str = samples_ans[rand.randint(0, len(samples_ans)-1)] if rand_data else ""
     random_three_str+=", ".join(rand_data)
